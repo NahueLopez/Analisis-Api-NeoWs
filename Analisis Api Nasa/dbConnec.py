bb937602658fa7441b674ad96da8c91dfc1a2136
Nahuel Luis López <78488563+NahueLopez@users.noreply.github.com>
2024-05-19 20:51:56 -0300

    Optimización, mejora de errores y datos

--- Analisis Api Nasa/dbConnec.py
+++ Analisis Api Nasa/dbConnec.py
@@ -21,6 +21,7 @@ def establecer_conexion():
 def verificar_tabla_existente(conn, table_name):
     try:
         with conn.cursor() as cur:
+            # Verifica si la tabla ya existe en la base de datos
             cur.execute(f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}');")
             exists = cur.fetchone()[0]
             return exists
@@ -30,38 +31,63 @@ def verificar_tabla_existente(conn, table_name):
 
 def crear_tabla_dinamica(conn, tabla):
     try:
-        cur = conn.cursor()
-        cur.execute(f"CREATE TABLE IF NOT EXISTS {tabla} ("
-                    f"id VARCHAR(255) PRIMARY KEY,"
-                    f"fecha_emision DATE,"
-                    f"name VARCHAR(255),"
-                    f"nasa_jpl_url VARCHAR(255),"
-                    f"absolute_magnitude_h FLOAT,"
-                    f"relative_velocity FLOAT,"
-                    f"miss_distance FLOAT,"
-                    f"estimated_diameter_min_kilometers FLOAT,"
-                    f"estimated_diameter_max_kilometers FLOAT"
-                    ");")
-        conn.commit()
-        print(f"Tabla '{tabla}' creada correctamente.")
+        # Define las columnas de la tabla
+        columnas = {
+            'id': 'VARCHAR(255) PRIMARY KEY',
+            'fecha_creacion': 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+            'fecha_emision': 'DATE',
+            'name': 'VARCHAR(255)',
+            'nasa_jpl_url': 'VARCHAR(255)',
+            'absolute_magnitude_h': 'FLOAT',
+            'relative_velocity': 'FLOAT',
+            'miss_distance': 'FLOAT',
+            'estimated_diameter_min_kilometers': 'FLOAT',
+            'estimated_diameter_max_kilometers': 'FLOAT'
+        }
+
+        # Construir la definición de las columnas
+        definicion_columnas = ", ".join([f"{nombre} {tipo}" for nombre, tipo in columnas.items()])
+
+        with conn.cursor() as cur:
+            # Crea la tabla si no existe
+            cur.execute(f"CREATE TABLE IF NOT EXISTS {tabla} ({definicion_columnas});")
+            conn.commit()
+            print(f"Tabla '{tabla}' creada correctamente.")
     except Exception as e:
         print(f"Error al crear la tabla: {e}")
 
-
-
 def insertar_datos(conn, tabla, values_list):
+    fechas_existentes = set()
+    datos_insertados = False
     try:
         with conn.cursor() as cur:
             for asteroid in values_list:
-                keys = list(asteroid.keys())
-                placeholders = ', '.join(['%s' for _ in range(len(keys))])
-                query = f"INSERT INTO {tabla} ({', '.join(keys)}) VALUES ({placeholders});"
-                values = [asteroid[key] for key in keys]
-                cur.execute(query, values)
+                fecha_emision = asteroid['fecha_emision']
+                id_asteroide = asteroid['id']
+                # Verifica si existen registros con la misma fecha de emisión y ID
+                cur.execute(f"SELECT COUNT(*) FROM {tabla} WHERE fecha_emision = %s AND id = %s", (fecha_emision, id_asteroide))
+                if cur.fetchone()[0] == 0:
+                    keys = list(asteroid.keys())
+                    placeholders = ', '.join(['%s' for _ in range(len(keys))])
+                    query = f"INSERT INTO {tabla} ({', '.join(keys)}) VALUES ({placeholders})"
+                    values = [asteroid[key] for key in keys]
+                    cur.execute(query, values)
+                    datos_insertados = True
+                else:
+                    fechas_existentes.add(fecha_emision)
         conn.commit()
-        print("Datos insertados correctamente.")
+        if datos_insertados:
+            print("Datos insertados correctamente.")
+        if fechas_existentes:
+            print(f"Las siguientes fechas de emisión ya tenían el ID registrado y no se agregaron nuevos registros para esos IDs: {', '.join(fechas_existentes)}")
+        if not datos_insertados and not fechas_existentes:
+            print("No se insertaron nuevos datos.")
     except Exception as e:
         print(f"Error al insertar datos en la tabla {tabla}: {e}")
+
+
+
+
 
 
 
