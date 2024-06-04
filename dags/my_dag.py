7e65807390ba4dc0c940bb747b9d0823a286584a
Nahuel Luis López <78488563+NahueLopez@users.noreply.github.com>
2024-06-03 22:04:37 -0300

    Update my_dag.py

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -11,12 +11,9 @@ from src.main import (
 )
 
 default_args = {
-    'owner': 'airflow',
+    'owner': 'Nahuel',
     'depends_on_past': False,
     'start_date': datetime(2023, 1, 1),
-    'email': ['tu_email@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
